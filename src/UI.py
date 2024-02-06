dac8d481d6dd88dd6a93d904415f71dabfc1a672
Pal, Aditya <adityapal@gatech.edu>
2024-02-06 10:23:48 -0500

    fix

--- src/UI.py
+++ src/UI.py
@@ -5,6 +5,7 @@ import run_auditor
 import run_critic
 import run_rank
 import shutil
+import time
 from utils import dotdict, clean_folder
 from streamlit_js_eval import streamlit_js_eval
 
@@ -40,216 +41,261 @@ if "start_auditor" not in st.session_state:
 if "start_ranking" not in st.session_state:
     st.session_state.start_ranking = False
 
-st.header("Auditor Step", divider=True)
-st.divider()
+if "section_active_auditor" not in st.session_state:
+    st.session_state.section_active_auditor = True
 
-col1, col2 = st.columns(2)
+if "section_active_critic" not in st.session_state:
+    st.session_state.section_active_critic = False
 
-with col1:
-    model = st.radio(
-        "Set the GPT model 👉",
-        key="model",
-        options=["gpt-3.5-turbo", "gpt-4", "gpt-4-turbo-preview"],
-        index=2
-    )
-    
-    uploaded_file = st.file_uploader('Upload data folder as a zip')
+if "section_active_ranking" not in st.session_state:
+    st.session_state.section_active_ranking = False
 
-with col2:
-    topk =  st.number_input(
-        "Set the topk auditor responses 👉",
-        key="topk",
-        min_value=1,
-        max_value=10,
-        value=3,
-        format="%d"
-    )
+if "args" not in st.session_state:
+    st.session_state.args = None
 
-    temperature = st.number_input(
-        "Set the temperature 👉",
-        key="temperature",
-        min_value=0.0,
-        max_value=1.0,
-        value=0.7,
-        format="%f"
-    )
+if "args_c" not in st.session_state:
+    st.session_state.args_c = None
 
-    num_auditors = st.number_input(
-        "Set the num auditors 👉",
-        key="num_auditors",
-        min_value=1,
-        max_value=10,
-        value=1,
-        format="%d"
-    )
+if "args_r" not in st.session_state:
+    st.session_state.args_r = None
 
-def start_auditor():
-    st.session_state.start_auditor = True
 
-st.button("Start Auditor", key="auditor", on_click=start_auditor)
+if st.session_state.section_active_auditor:
 
-if st.session_state.start_auditor:
-    if uploaded_file:
-        os.environ["OPENAI_API_KEY"] = openai_api_key
-        args_dict = {
-            'backend': model,
-            'temperature': temperature,
+    st.header("Auditor Step", divider=True)
+    st.divider()
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        model = st.radio(
+            "Set the GPT model 👉",
+            key="model",
+            options=["gpt-3.5-turbo", "gpt-4", "gpt-4-turbo-preview"],
+            index=2
+        )
+        
+        uploaded_file = st.file_uploader('Upload data folder as a zip')
+
+    with col2:
+        topk =  st.number_input(
+            "Set the topk auditor responses 👉",
+            key="topk",
+            min_value=1,
+            max_value=10,
+            value=3,
+            format="%d"
+        )
+
+        temperature = st.number_input(
+            "Set the temperature 👉",
+            key="temperature",
+            min_value=0.0,
+            max_value=1.0,
+            value=0.7,
+            format="%f"
+        )
+
+        num_auditors = st.number_input(
+            "Set the num auditors 👉",
+            key="num_auditors",
+            min_value=1,
+            max_value=10,
+            value=1,
+            format="%d"
+        )
+
+    def start_auditor():
+        st.session_state.start_auditor = True
+    def end_auditor():
+        st.session_state.start_auditor = False
+
+    audit_button = st.button("Start Auditor", key="auditor", on_click=start_auditor)
+
+    if audit_button and st.session_state.start_auditor:
+        if uploaded_file:
+            os.environ["OPENAI_API_KEY"] = openai_api_key
+            args_dict = {
+                'backend': model,
+                'temperature': temperature,
+                'dataset': "CVE",
+                'topk': topk,
+                'num_auditor': num_auditors,
+                'openai_api_key': openai_api_key
+            }
+            args = dotdict(args_dict)
+            st.session_state.args = args
+            if os.path.exists("data"):
+                clean_folder("data")
+            if os.path.exists(f"src/logs/auditor_{args.backend}_{args.temperature}_top{args.topk}_{args.num_auditor}"):
+                clean_folder(f"src/logs/auditor_{args.backend}_{args.temperature}_top{args.topk}_{args.num_auditor}")
+            with open("data.zip", 'wb') as bin_file:
+                bin_file.write(uploaded_file.read())
+            with zipfile.ZipFile("data.zip", 'r') as zip_ref:
+                zip_ref.extractall()
+            st.write("Starting auditor code!")
+            run_auditor.mainfnc(args)
+            st.write(f"Audit files processed successfully to folder ./src/logs/auditor_{args.backend}_{args.temperature}_top{args.topk}_{args.num_auditor}!")
+            end_auditor()
+            time.sleep(2)
+            # st.session_state.section_active_auditor = False
+            st.session_state.section_active_critic= True
+            uploaded_file = False
+        else:
+            st.warning("Please upload data zip.")
+            st.stop()
+    # else:
+    #     st.stop()
+
+
+
+if st.session_state.section_active_critic:
+
+    st.header("Critic Step", divider=True)
+    st.divider()
+
+    col1, col2 = st.columns(2)
+
+    args = st.session_state.args
+
+    with col1:
+        model_c = st.radio(
+            "Set the GPT model 👉",
+            key="model_c",
+            options=["gpt-3.5-turbo", "gpt-4", "gpt-4-turbo-preview"],
+            index=2
+        )
+        
+        auditor_dir_c = st.text_input(
+            "Auditor Directory location",
+            value=f"auditor_{args.backend}_{args.temperature}_top{args.topk}_{args.num_auditor}"
+        )
+
+    with col2:
+
+        temperature_c = st.number_input(
+            "Set the temperature 👉",
+            key="temperature_c",
+            min_value=0.0,
+            max_value=1.0,
+            value=0.0,
+            format="%f"
+        )
+
+        num_critic_c = st.number_input(
+            "Set the num critics 👉",
+            key="num_critic_c",
+            min_value=1,
+            max_value=10,
+            value=1,
+            format="%d"
+        )
+
+        shot_c = st.radio(
+            "Set the num shots (few/one) 👉",
+            key="shot_c",
+            options=["one", "few"],
+            index=1
+        )
+
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+
+    def start_critic():
+        st.session_state.start_critic = True
+    def end_critic():
+        st.session_state.start_critic = False
+
+    critic_button = st.button("Start Critics", key="critic", on_click=start_critic)
+
+    if critic_button and st.session_state.start_critic:
+        print ("Entered")
+        st.write(f"Entered!")
+        args_c_dict = {
+            'backend': model_c,
+            'temperature': temperature_c,
             'dataset': "CVE",
-            'topk': topk,
-            'num_auditor': num_auditors,
+            'auditor_dir': auditor_dir_c,
+            'num_critic': num_critic_c,
+            'shot': shot_c,
             'openai_api_key': openai_api_key
         }
-        args = dotdict(args_dict)
-        if os.path.exists("data"):
-            clean_folder("data")
-        if os.path.exists(f"src/logs/auditor_{args.backend}_{args.temperature}_top{args.topk}_{args.num_auditor}"):
-            clean_folder(f"src/logs/auditor_{args.backend}_{args.temperature}_top{args.topk}_{args.num_auditor}")
-        with open("data.zip", 'wb') as bin_file:
-            bin_file.write(uploaded_file.read())
-        with zipfile.ZipFile("data.zip", 'r') as zip_ref:
-            zip_ref.extractall()
-        st.write("Starting auditor code!")
-        run_auditor.mainfnc(args)
-        st.write(f"Audit files processed successfully to folder ./src/logs/auditor_{args.backend}_{args.temperature}_top{args.topk}_{args.num_auditor}!")
+        args_c = dotdict(args_c_dict)
+        st.session_state.args_c = args_c
+        st.write("Starting critic code!")
+        run_critic.mainfnc(args_c)
+        st.write(f"Critic files processed successfully to folder ./src/logs/{args_c.auditor_dir}/critic_{args_c.backend}_{args_c.temperature}_{args_c.num_critic}_{args_c.shot}!")
+        end_critic()
+        time.sleep(2)
+        # st.session_state.section_active_critic = False
+        st.session_state.section_active_ranking = True
+    # else:
+    #     st.stop()
+
+
+
+if st.session_state.section_active_ranking:
+    st.header("Ranking Step", divider=True)
+    st.divider()
+
+    col1, col2 = st.columns(2)
+
+    args = st.session_state.args
+    args_c = st.session_state.args_c
+
+    with col1:
+        
+        auditor_dir_r = st.text_input(
+            "Auditor Dir location",
+            value=f"auditor_{args.backend}_{args.temperature}_top{args.topk}_{args.num_auditor}"
+        )
+
+        critic_dir_r = st.text_input(
+            "Critic Directory location",
+            value=f"critic_{args_c.backend}_{args_c.temperature}_{args_c.num_critic}_{args_c.shot}"
+        )
+
+    with col2:
+
+        strategy_r = st.radio(
+            "Set the strategy (default/custom) 👉",
+            key="strategy_r",
+            options=["default", "custom"],
+            index=0
+        )
+
+    def start_ranking():
+        st.session_state.start_ranking = True
+    def end_ranking():
+        st.session_state.start_ranking = False
+
+    rank_button = st.button("Start Ranking", key="ranking", on_click=start_ranking)
+
+    if rank_button and st.session_state.start_ranking:
+        args_r_dict = {
+            'auditor_dir': auditor_dir_r,
+            'critic_dir': critic_dir_r,
+            'strategy': strategy_r
+        }
+        args_r = dotdict(args_r_dict)
+        st.session_state.args_r = args_r
+        st.write(f"Starting ranking code!")
+        run_rank.mainfnc(args_r)
+        st.write(f"Ranking files processed successfully to folder ./src/logs/{args_c.auditor_dir}/critic_{args_c.backend}_{args_c.temperature}_{args_c.num_critic}_{args_c.shot}/ranker_{args_r.strategy}!")
+        end_critic()
     else:
-        st.warning("Please upload data zip.")
         st.stop()
-else:
-    st.stop()
 
+    shutil.make_archive('results', 'zip', "src/logs")
 
+    st.divider()
 
+    with open("results.zip", "rb") as fp:
+        download_btn = st.download_button(
+            label="Download Results zip",
+            data=fp,
+            file_name="results.zip",
+            mime="application/zip"
+        )
 
-
-st.header("Critic Step", divider=True)
-st.divider()
-
-col1, col2 = st.columns(2)
-
-with col1:
-    model_c = st.radio(
-        "Set the GPT model 👉",
-        key="model_c",
-        options=["gpt-3.5-turbo", "gpt-4", "gpt-4-turbo-preview"],
-        index=2
-    )
-    
-    auditor_dir_c = st.text_input(
-        "Auditor Directory location",
-        value=f"auditor_{args.backend}_{args.temperature}_top{args.topk}_{args.num_auditor}"
-    )
-
-with col2:
-
-    temperature_c = st.number_input(
-        "Set the temperature 👉",
-        key="temperature_c",
-        min_value=0.0,
-        max_value=1.0,
-        value=0.0,
-        format="%f"
-    )
-
-    num_critic_c = st.number_input(
-        "Set the num critics 👉",
-        key="num_critic_c",
-        min_value=1,
-        max_value=10,
-        value=1,
-        format="%d"
-    )
-
-    shot_c = st.radio(
-        "Set the num shots (few/one) 👉",
-        key="shot_c",
-        options=["one", "few"],
-        index=1
-    )
-
-os.environ["OPENAI_API_KEY"] = openai_api_key
-
-def start_critic():
-    st.session_state.start_critic = True
-
-st.button("Start Critics", key="critic", on_click=start_critic)
-
-if st.session_state.start_critic:
-    print ("Entered")
-    st.write(f"Entered!")
-    args_c_dict = {
-        'backend': model_c,
-        'temperature': temperature_c,
-        'dataset': "CVE",
-        'auditor_dir': auditor_dir_c,
-        'num_critic': num_critic_c,
-        'shot': shot_c,
-        'openai_api_key': openai_api_key
-    }
-    args_c = dotdict(args_c_dict)
-    st.write("Starting critic code!")
-    run_critic.mainfnc(args_c)
-    st.write(f"Critic files processed successfully to folder ./src/logs/{args_c.auditor_dir}/critic_{args_c.backend}_{args_c.temperature}_{args_c.num_critic}_{args_c.shot}!")
-else:
-    st.stop()
-
-
-
-
-st.header("Ranking Step", divider=True)
-st.divider()
-
-col1, col2 = st.columns(2)
-
-with col1:
-    
-    auditor_dir_r = st.text_input(
-        "Auditor Dir location",
-        value=f"auditor_{args.backend}_{args.temperature}_top{args.topk}_{args.num_auditor}"
-    )
-
-    critic_dir_r = st.text_input(
-        "Critic Directory location",
-        value=f"critic_{args_c.backend}_{args_c.temperature}_{args_c.num_critic}_{args_c.shot}"
-    )
-
-with col2:
-
-    strategy_r = st.radio(
-        "Set the strategy (default/custom) 👉",
-        key="strategy_r",
-        options=["default", "custom"],
-        index=0
-    )
-
-def start_ranking():
-    st.session_state.start_ranking = True
-
-st.button("Start Ranking", key="ranking", on_click=start_ranking)
-
-if st.session_state.start_ranking:
-    args_r_dict = {
-        'auditor_dir': auditor_dir_r,
-        'critic_dir': critic_dir_r,
-        'strategy': strategy_r
-    }
-    args_r = dotdict(args_r_dict)
-    st.write(f"Starting ranking code!")
-    run_rank.mainfnc(args_r)
-    st.write(f"Ranking files processed successfully to folder ./src/logs/{args_c.auditor_dir}/critic_{args_c.backend}_{args_c.temperature}_{args_c.num_critic}_{args_c.shot}/ranker_{args_r.strategy}!")
-else:
-    st.stop()
-
-shutil.make_archive('results', 'zip', "src/logs")
-
-st.divider()
-
-with open("results.zip", "rb") as fp:
-    download_btn = st.download_button(
-        label="Download Results zip",
-        data=fp,
-        file_name="results.zip",
-        mime="application/zip"
-    )
-
-if st.button("Reset"):
-    streamlit_js_eval(js_expressions="parent.window.location.reload()")
+    if st.button("Reset"):
+        streamlit_js_eval(js_expressions="parent.window.location.reload()")
