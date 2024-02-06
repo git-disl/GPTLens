b984b3b091b5ad1a5b46180cfee6f24bf86b88fd
Pal, Aditya <adityapal@gatech.edu>
2024-02-06 01:25:25 -0500

    fixes

--- src/UI.py
+++ src/UI.py
@@ -8,6 +8,8 @@ import shutil
 from utils import dotdict, clean_folder
 import pyautogui
 
+os.environ['DISPLAY'] = ':0'
+
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
