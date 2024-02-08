9c28050ef4af8f8b6429c2987e4d98ff4825cf05
Pal, Aditya <adityapal@gatech.edu>
2024-02-08 00:21:45 -0500

    fix code links

--- src/UI.py
+++ src/UI.py
@@ -15,8 +15,8 @@ os.environ['DISPLAY'] = ':0'
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    "[View the source code](https://github.com/sciencepal/GPTLens/blob/aditya-test/src/UI.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/sciencepal/GPTLens?quickstart=1)"
     st.divider()
     if st.button("Reset App"):
         streamlit_js_eval(js_expressions="parent.window.location.reload()")
